d7ba51d97fe0a5ae1624b070b19b3f3a7b39a2f9
MarisaPeer <85636554+MarisaPeer@users.noreply.github.com>
2021-08-27 11:43:06 +0100

    27/08/2021

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// RTT Kustomer Chat Widget
+!function(a,b,c,d){a.Kustomer=c,c._q=[],c._i=[],c.init=function(a){function b(a,b){a[b]=function(){a._q.push([b].concat(Array.prototype.slice.call(arguments,0)))}}for(var d="init clear identify track start describe on".split(" "),e=0;e<d.length;e++)b(c,d[e]);c._i.push(a)};var e=b.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.kustomerapp.com/cw/sdk.v1.1.min.js";var f=b.getElementsByTagName("script")[0];f.parentNode.insertBefore(e,f)}(this,document,window.Kustomer||{});
+
+// RTT Kustomer API Key
+Kustomer.init('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNTIyYjcyZmQ2NmYyMDA5NTk4OTU1NCIsInVzZXIiOiI2MDUyMmI3MTQ2ZWQzMDc0ZWY1MjkzN2QiLCJvcmciOiI1Zjc3NjcyYTdkMDFiYTIwNjQwMjg5MGIiLCJvcmdOYW1lIjoibWFyaXNhcGVlciIsInVzZXJUeXBlIjoibWFjaGluZSIsInBvZCI6InByb2QyIiwicm9sZXMiOlsib3JnLnRyYWNraW5nIl0sImF1ZCI6InVybjpjb25zdW1lciIsImlzcyI6InVybjphcGkiLCJzdWIiOiI2MDUyMmI3MTQ2ZWQzMDc0ZWY1MjkzN2QifQ.km3r6Xe-9dcbo7NobrPljx3X6CpNga55aenfquwPc24');
+
+// Start Widget
+Kustomer.start();
